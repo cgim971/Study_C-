d86a5bcc981bab733c5c79a4b3e02dad707d682c
ChanheeKim <cgim971@gmail.com>
2023-02-15 23:13:45 +0900

    백준_16395
    파스칼의 삼각형

---
+++ Study_C++/Study_C++/16395_파스칼의삼각형.cpp
@@ -0,0 +1,19 @@
+#include<iostream>	
+using namespace std;
+
+int arr[31][31] = { 1 };
+
+int  main() {
+
+	int n, k;
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i + 1; j++) {
+			arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+		}
+	}
+
+	cout << arr[n][k];
+	return 0;
+}
